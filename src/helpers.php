4c9ec6b2e447c6e616550410368da8516417e6fe
Jagepard <jagepard@yandex.ru>
2025-05-21 20:06:27 +0300

    update

--- src/helpers.php
+++ src/helpers.php
@@ -1,24 +1,21 @@
-<?php
+<?php 
+
+declare(strict_types=1);
+
+/**
+ * @author  : Jagepard <jagepard@yandex.ru>
+ * @license https://mit-license.org/  MIT
+ */
 
 use Rudra\View\ViewFacade as View;
 
 if (!function_exists('view')) {
     /**
-     * Returns the html representation from the output buffer
-     * Imports variables from the $data array into the template
-     * When passing an array:
-     *     the html view is created according to the first parameter
-     *     and cache file according to the second
-     * -------------------------------------------------------------------
-     * Возвращает html представление из буфера вывода
-     * Импортирует переменные из массива $data в шаблон
-     * При передаче массива:
-     *     создается html представление в соответствии с первым параметром 
-     *     и файл кэша в соответствии со вторым
-     * 
-     * @param  [type]       $path
-     * @param  array        $data
-     * @return string|false
+     * Renders a template and returns the result as a string.
+     *
+     * @param string|array $path
+     * @param array        $data
+     * @return string
      */
     function view($path, array $data = []): string
     {
@@ -28,15 +25,12 @@ if (!function_exists('view')) {
 
 if (!function_exists('render')) {
     /**
-     * Displays template
-     * -----------------
-     * Отображает шаблон
+     * Renders and outputs a template directly.
      *
-     * @param  [type] $path
-     * @param  array  $data
-     * @return void
+     * @param string|array $path
+     * @param array        $data
      */
-    function render($path, array $data = [])
+    function render($path, array $data = []): void
     {
         echo View::view($path, $data);
     }
@@ -44,31 +38,13 @@ if (!function_exists('render')) {
 
 if (!function_exists('cache')) {
     /**
-     * Checks the cache file against the caching frequency,
-     * if the cache is out of date, then it must be updated
-     * The default caching interval time is specified in the configuration file
-     * config/setting.local.yml for local development config/setting.production.yml for public release
-     * Example: 'cache.time' => 'now',
-     * The caching interval time can be passed as the second parameter $time
-     * Example: '+1 day'
-     * The value of $fullPage true interrupts further code processing if the data in the cache file is up-to-date
-     * Example: cache('index', '+1 day', true); or cache('index', null, true);
-     * ----------------------------------------------------------------------------------------------------------
-     * Проверяет файл кэша на соответствие периодичности кэширования,
-     * если кеш устарел, то он должен быть обновлен
-     * Время периодичности кэширования по умолчанию указывается в файле конфигурации 
-     * config/setting.local.yml для локальной разработки config/setting.production.yml для публичного размещения
-     * Пример: 'cache.time' => 'now', 
-     * Время периодичности кэширования можно передать вторым элементом массива $path[1]
-     * Пример: '+1 day'
-     * Значение $fullPage true прерывает дальнейшую обработку кода в случае актуальности данных в файле кэша
-     * Пример: cache(['index', '+1 day'], true); или cache(['index'], true); // время  из конфигурвции
+     * Checks if a cached version is valid.
      *
-     * @param  string      $path
-     * @param  boolean     $fullPage
+     * @param array   $path
+     * @param boolean $fullPage
      * @return string|null
      */
-    function cache(array $path, $fullPage = false)
+    function cache(array $path, $fullPage = false): ?string
     {
         return View::cache($path, $fullPage);
     }
